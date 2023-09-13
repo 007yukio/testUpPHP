0477e0505ce949383113f71a7ca8032b6225469f
007Y <tenten0407team@yahoo.co.jp>
2023-09-14 01:03:47 +0900

    今日も書いてみた

---
+++ PHP試験対策/test02.php
@@ -0,0 +1,34 @@
+<?php
+
+$numbers = array(10,20,30);
+$numbers[] = 40;
+$numbers[] = 50;
+print ($numbers[4].PHP_EOL);
+
+$colors['sea']   = 'blue';
+$colors['leaf']  = 'green';
+$colors['night'] = 'black';
+print_r($colors['sea'].PHP_EOL);
+print_r($colors['leaf'].PHP_EOL);
+print_r($colors['night'].PHP_EOL);
+
+$colors = array(
+    '国語'=>80,
+    '数学'=>90,
+    '英語'=>75
+);
+print_r($colors.PHP_EOL);
+
+echo ("============================".PHP_EOL);
+
+$colors[0]='red';
+$colors[1]='yellow';
+$colors[3]='orange';
+$colors[2]='blue';
+
+foreach($colors as $color){
+    print ("$color".PHP_EOL);
+}
+
+echo "============================".PHP_EOL;
+
